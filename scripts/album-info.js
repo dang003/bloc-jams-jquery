1f0dece368cdbc02e74fa219ad492f5350231bf0
DanG <gallagherd07@gmail.com>
2020-01-26 14:13:40 -0600

    Checkpoint 15: complete

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,6 +1,4 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
-  $('.artist').text(album.artist);
-  $('#release-info').text(album.releaseInfo);
 }

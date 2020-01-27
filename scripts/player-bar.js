79d10305d4443a2fe80c13fe9e5515fb82d13782
DanG <gallagherd07@gmail.com>
2020-01-27 11:52:54 -0600

    Checkpoint 17: complete

---
+++ scripts/player-bar.js
@@ -0,0 +1,19 @@
+{
+  $('button#play-pause').on('click', function() {
+    player.playPause();
+    $(this).attr('playState', player.playState);
+  });
+
+  $('button#next').on('click', function () {
+    if (player.playState !== 'playing') { return; }
+
+    const CURRENT_SONG_INDEX = album.songs.indexOf(player.currentlyPlaying);
+    const NEXT_SONG_INDEX = CURRENT_SONG_INDEX + 1;
+    if (NEXT_SONG_INDEX >= album.songs.length) { return; }
+
+    const NEXT_SONG = album.songs[NEXT_SONG_INDEX];
+    player.playPause(NEXT_SONG);
+
+  });
+
+}
